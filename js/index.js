dab0ce30b3de735ced131e96c8f004aef80bf144
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-12-17 11:18:00 +0300

    created variables for the API's, and variables to get the elements by id

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const INGREDIENT = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast'
+
+const MEAL = 'https://www.themealdb.com/api/json/v1/1/random.php'
+
+const SEARCH = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+
+// gets the CARD DATA elements 
+const mealCard = document.getElementById('meal-cardBody')
+const ingredientCard = document.getElementById('meal-ingredients')
+const searchRowCard = document.getElementById('search-result')
+
+// gets the CARD DATA elements 
+const homeLink = document.getElementById('home-link')
+const ingredientsLink = document.getElementById('ingredient-link')
+const dropdownLink = document.getElementById('dropdown-link"')
+
+// get search form data elements 
+const searchForm = document.getElementById('search-form')
+const searchInput = document.getElementById('search-input')
