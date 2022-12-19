1031a91bc9b2909b967a5e453bf6955122310341
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-12-19 20:40:01 +0300

    added pop up effect for the authentification form and changed its positioning

--- js/index.js
+++ js/index.js
@@ -2,59 +2,63 @@ const INGREDIENT = 'https://www.themealdb.com/api/json/v1/1/filter.php?i='
 
 const MEAL = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
 
-const CATEGORY = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list'
+const RANDOM = 'https://www.themealdb.com/api/json/v1/1/random.php'
+
+const CATEGORY = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list'
 
 const SEARCH = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadMenu()
-// Login Form details and events
-const mainNavbar = document.getElementById('mainNav')
-const form_Container = document.getElementById('formContainer')
-const section_1 = document.getElementById('section1')
+    loadRandom()
+    // loadMenu()
+    // Login Form details and events
+    const mainNavbar = document.getElementById('mainNav')
+    const form_Container = document.getElementById('formContainer')
+    const section_1 = document.getElementById('section1')
 
-const signupBtn = document.getElementById('signupBtn')
-const signinBtn = document.getElementById('signinBtn')
-const nameField = document.getElementById('nameField')
-const signupTitle = document.getElementById('signupTitle')
+    const signupBtn = document.getElementById('signupBtn')
+    const signinBtn = document.getElementById('signinBtn')
+    const nameField = document.getElementById('nameField')
+    const signupTitle = document.getElementById('signupTitle')
 
-signinBtn.onclick = function(){
-    nameField.style.maxHeight = '0'
-    signupTitle.innerHTML = 'Sign In'
-    signupBtn.classList.add('disable')
-    signinBtn.classList.remove('disable')
-    signinBtn.addEventListener('click', () => {
-        alert('Successfully Signed In')
-        mainNavbar.removeAttribute('hidden')
-        mealCard.removeAttribute('hidden')
-        section_1.removeAttribute('hidden')
-        searchForm.removeAttribute('hidden')
-        searchMeal()
-        loadMenu()
-        loadIngredients()
-        loadCategory()
-        form_Container.style.display = "none"
-    })
-}
+    signinBtn.onclick = function(){
+        nameField.style.maxHeight = '0'
+        signupTitle.innerHTML = 'Sign In'
+        signupBtn.classList.add('disable')
+        signinBtn.classList.remove('disable')
+        signinBtn.addEventListener('click', () => {
+            alert('Successfully Signed In')
+            mainNavbar.removeAttribute('hidden')
+            mealCard.removeAttribute('hidden')
+            section_1.removeAttribute('hidden')
+            searchForm.removeAttribute('hidden')
+            searchMeal()
+            loadMenu()
+            loadIngredients()
+            // loadCategory()
+            form_Container.style.display = "none"
+        })
+    }
 
-signupBtn.onclick = function(){
-    nameField.style.maxHeight = '60px'
-    signupTitle.innerHTML = 'Sign Up'
-    signupBtn.classList.remove('disable')
-    signinBtn.classList.add('disable')
-    signupBtn.addEventListener('click', () => {
-        alert('Successfully Signed Up')
-        mainNavbar.removeAttribute('hidden')
-        mealCard.removeAttribute('hidden')
-        section_1.removeAttribute('hidden')
-        searchForm.removeAttribute('hidden')
-        searchMeal()
-        loadMenu()
-        loadIngredients()
-        loadCategory()
-        form_Container.style.display = "none"
-    })
-}
+    signupBtn.onclick = function(){
+        nameField.style.maxHeight = '60px'
+        signupTitle.innerHTML = 'Sign Up'
+        signupBtn.classList.remove('disable')
+        signinBtn.classList.add('disable')
+        signupBtn.addEventListener('click', () => {
+            alert('Successfully Signed Up')
+            mainNavbar.removeAttribute('hidden')
+            mealCard.removeAttribute('hidden')
+            section_1.removeAttribute('hidden')
+            searchForm.removeAttribute('hidden')
+            searchMeal()
+            loadMenu()
+            loadIngredients()
+            // loadCategory()
+            form_Container.style.display = "none"
+        })
+    }
+})
 
 // Feedback Form details and events
 const rateTitle = document.getElementById('rate-title')
@@ -157,9 +161,10 @@ function creatingMeal (image, id, category, title, instructions, link) {
     mealCategory.innerText = category
 
     const mealInstructions = document.createElement('div')
-    mealInstructions.classList.add('card-text')
+    mealInstructions.classList.add('card-text', 'hiddenText', 'visibleText')
     mealInstructions.innerText = instructions
     mealInstructions.style.display = 'none'
+
 
     const readBtn = document.createElement('button')
     readBtn.classList.add('mt-1', 'mb-2', 'me-1', 'ms-5', 'btn', 'btn-warning')
@@ -240,8 +245,28 @@ function loadMenu () {
         })
 }
 
+function loadRandom () {
+    fetch(RANDOM)
+        .then((response) => response.json())
+        .then((data) => {
+            const meal = data.meals
+            meal.forEach(mealData => {
+            const image = mealData.strMealThumb
+            const id = mealData.idMeal
+            const category = mealData.strCategory
+            const title = mealData.strMeal
+            const instructions = mealData.strInstructions
+            const link = mealData.strYoutube
+
+            const mealElement = creatingMeal(image, id, category, title, instructions, link)
+
+            mealCard.appendChild(mealElement)
+            })
+        })
+}
+
 // creating ingredients element
-function creatingIngredients (image, id, title) {
+function creatingIngredients (id, ingredient, description) {
     const rootDiv = document.createElement('div')
     rootDiv.classList.add('card', 'u-clearfix', 'col-12', 'px-0', 'mb-3')
 
@@ -251,22 +276,25 @@ function creatingIngredients (image, id, title) {
     const cardDiv = document.createElement('div')
     cardDiv.classList.add('card-body', 'col-6')
 
-    const imageDiv = document.createElement('div')
-    imageDiv.classList.add( 'col-6')
-
-    const mealImage = document.createElement('img')
-    mealImage.classList.add('card-media','h-100')
-    mealImage.src = image
-    mealImage.objectFit = 'cover'
-    mealImage.float = 'right'
-
     const mealId = document.createElement('span')
     mealId.classList.add('card-number', 'card-circle', 'subtle')
     mealId.innerText = id
 
     const mealTitle = document.createElement('h2')
     mealTitle.classList.add('card-title')
-    mealTitle.innerText = title
+    mealTitle.innerText = ingredient
+
+    const mealInstructions = document.createElement('div')
+    mealInstructions.classList.add('card-text')
+    mealInstructions.innerText = description
+    mealInstructions.style.display = 'none'
+
+    const readBtn = document.createElement('button')
+    readBtn.classList.add('mt-1', 'mb-2', 'me-1', 'ms-5', 'btn', 'btn-warning')
+    readBtn.innerText = 'READ'
+    readBtn.addEventListener('click', () => {
+        mealInstructions.style.display = 'block'
+        readBtn.style.display = 'none'
 
     // const likebutton = document.createElement('h5')
     // // const likebutton = document.createAttribute("button")
@@ -277,17 +305,16 @@ function creatingIngredients (image, id, title) {
     rootDiv.appendChild(rowDiv)
     rootDiv.appendChild(cardDiv)
 
-    imageDiv.appendChild(mealImage)
-
     cardDiv.appendChild(mealId)
     cardDiv.appendChild(mealTitle)
+    cardDiv.appendChild(mealInstructions)
     // cardDiv.appendChild(likebutton)
 
 
     return rootDiv
 
 }
-
+)}
 function loadIngredients () {
     fetch(INGREDIENT)
         .then((response) => response.json())
@@ -298,71 +325,6 @@ function loadIngredients () {
             )
             ingredientCard.append(...ingrElement)
         })
-}
-
-
-// creating search results
-function creatingSearch(image, id, category, title, instructions, link) {
-    const rootDiv = document.createElement('div')
-    rootDiv.classList.add('card', 'u-clearfix', 'col-12', 'px-0', 'mb-3')
-
-    const rowDiv = document.createElement('div')
-    rowDiv.classList.add('row')
-
-    const cardDiv = document.createElement('div')
-    cardDiv.classList.add('card-body', 'col-6')
-
-    const imageDiv = document.createElement('div')
-    imageDiv.classList.add( 'col-6')
-
-    const mealImage = document.createElement('img')
-    mealImage.classList.add('card-media','h-100')
-    mealImage.src = image
-    mealImage.objectFit = 'cover'
-    mealImage.float = 'right'
-
-    const mealId = document.createElement('span')
-    mealId.classList.add('card-number', 'card-circle', 'subtle')
-    mealId.innerText = id
-
-    const mealCategory = document.createElement('span')
-    mealCategory.classList.add('card-category')
-    mealCategory.innerText = category
-
-    const mealTitle = document.createElement('h2')
-    mealTitle.classList.add('card-title')
-    mealTitle.innerText = title
-
-    const mealInstructions = document.createElement('p')
-    mealInstructions.classList.add('card-description')
-    mealInstructions.innerText = instructions
-
-    const mealLink = document.createElement('a')
-    mealLink.classList.add('mt-1', 'mb-2', 'me-3', 'ms-5', 'btn', 'btn-warning')
-    mealLink.innerText = 'Video ...'
-    mealLink.href = link
-    mealLink.target = '_blank'
-
-    // cardDiv.appendChild(mealImage)
-
-    cardDiv.appendChild(mealId)
-    cardDiv.appendChild(mealTitle)
-    cardDiv.appendChild(mealLink)
-    cardDiv.appendChild(mealCategory)
-
-
-    // append image element to image div
-    imageDiv.appendChild(mealImage)
-
-    // append divs to row
-    rowDiv.appendChild(cardDiv)
-    rowDiv.appendChild(imageDiv)
-
-    // append the row to the root
-    rootDiv.appendChild(rowDiv)
-
-    return rootDiv
-
 }
 
 // search data
@@ -379,7 +341,7 @@ function searchMeal (meal) {
                     const category = mealData.strCategory
                     const instructions = mealData.strInstructions
                     const link = mealData.strYoutube
-                    return creatingSearch(image, id, category, title, instructions, link)
+                    return creatingMeal(image, id, category, title, instructions, link)
                 }
             )
             // replace all children
@@ -388,35 +350,35 @@ function searchMeal (meal) {
 }
 
  // create category element
- function createCategory (category) {
-    // const rootDiv = document.createElement('div')
-    // rootDiv.classList.add('col-3', 'p-1')
+//  function createCategory (category) {
+//     // const rootDiv = document.createElement('div')
+//     // rootDiv.classList.add('col-3', 'p-1')
 
-    // const spanData = document.createElement('span')
-    // spanData.classList.add('col-12', 'badge', 'text-bg-success', 'p-2')
-    // spanData.innerText = country
+//     // const spanData = document.createElement('span')
+//     // spanData.classList.add('col-12', 'badge', 'text-bg-success', 'p-2')
+//     // spanData.innerText = country
 
-    const rootDiv = document.createElement('div')
-    rootDiv.classList.add("span")
+//     const rootDiv = document.createElement('div')
+//     rootDiv.classList.add("span")
 
-    const liElem = document.querySelector('#category-list')
-    liElem.innerText = category
+//     const liElem = document.querySelector('#category-list')
+//     liElem.innerText = category
 
-    rootDiv.appendChild(liElem)
-    return rootDiv
-}
+//     rootDiv.appendChild(liElem)
+//     return rootDiv
+// }
 
-function loadCategory () {
-    fetch(CATEGORY)
-        .then(response => response.json())
-        .then(data => {
-            const categoryData = data.categories
-            const elements = categoryData.map(
-                cat => createCategory(cat.strCategory)
-            )
-            categoryRow.append(...elements)
-        })
-}
+// function loadCategory () {
+//     fetch(CATEGORY)
+//         .then(response => response.json())
+//         .then(data => {
+//             const categoryData = data.meals
+//             const elements = categoryData.map(
+//                 cat => createCategory(cat.idIngredient, cat.strIngredient, cat.strDescription)
+//             )
+//             categoryRow.append(...elements)
+//         })
+// }
 
 
     // document.addEventListener('DOMContentLoaded', () => {
@@ -425,7 +387,7 @@ function loadCategory () {
     //     loadCategory()
     // })
 
-})
+
 
 // function creatingLikes (meals){
 //     const likebutton = document.createAttribute("button")
