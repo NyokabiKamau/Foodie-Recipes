722ffb4c108c8951b78aa4f919ff1764ff2a01a4
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-12-20 23:23:30 +0300

    made changes to the fetched data, navbar and card meal display

--- js/index.js
+++ js/index.js
@@ -12,6 +12,8 @@ const SEARCH = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
 document.addEventListener('DOMContentLoaded', () => {
     loadRandom()
     // loadMenu()
+
+
     // Login Form details and events
     const form_Container = document.getElementById('formContainer')
 
@@ -29,8 +31,6 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('Successfully Signed In')
             searchMeal()
             loadMenu()
-            loadIngredients()
-            loadCategory()
             form_Container.style.display = "none"
         })
     }
@@ -44,12 +44,12 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('Successfully Signed Up')
             searchMeal()
             loadMenu()
-            loadIngredients()
-            loadCategory()
             form_Container.style.display = "none"
         })
     }
 })
+
+
 
 // Feedback Form details and events
 const rateTitle = document.getElementById('rate-title')
@@ -61,62 +61,30 @@ const rateComment = document.getElementById('comment-Form')
     })
 
 
+
 // ABOUT SECTION
-const about = document.getElementById('about-link')
-about.addEventListener('click', () => {
+const aboutLink = document.getElementById('about-link')
+const about = document.getElementById('aboutId')
+
+aboutLink.addEventListener('click', () => {
     about.removeAttribute('hidden')
     about.style.display = "block"
     mealCard.style.display = "none"
     searchRowCard.style.display = "none"
-    ingredientCard.style.display = "none"
     
 })
 
 
 // gets the CARD DATA elements 
 const mealCard = document.getElementById('meal-cardBody')
-const ingredientCard = document.getElementById('meal-ingredients')
-const searchRowCard = document.getElementById('search-result')
-const categoryRow = document.getElementById('categories')
+const recipeCard = document.getElementById('recipe-cardBody')
 
-// gets the CARD DATA elements 
-const homeLink = document.getElementById('home-link')
-const ingredientsLink = document.getElementById('ingredient-link')
-const categoryLink = document.getElementById('category-list')
+
 
 // get search form data elements 
+const searchRowCard = document.getElementById('search-result')
 const searchForm = document.getElementById('search-form')
 const searchInput = document.getElementById('search-input')
-
-// click event for ingredient
-ingredientsLink.addEventListener('click', () => {
-    mealCard.style.display = "none"
-    searchRowCard.style.display = "none"
-    categoryRow.style.display = "none"
-    ingredientCard.removeAttribute('hidden')
-    ingredientCard.style.display = "block"
-    about.style.display = "none"
-    loadIngredients()
-})
-
-// click event for categories
-categoryLink.addEventListener('click', event => {
-    mealCard.style.display = "none"
-    searchRowCard.style.display = "none"
-    ingredientCard.style.display = "none"
-    categoryRow.style.display = "block"
-    categoryRow.removeAttribute('hidden')
-    loadCategory()
-})
-
-// click event for home link
-homeLink.addEventListener('click', () => {
-    searchRowCard.style.display = "block"
-    ingredientCard.style.display = "none"
-    categoryLink.style.display = "none"
-    mealCard.style.display = "block"
-    about.style.display = "none"
-})
 
 // submit event for search form
 searchForm.addEventListener('submit', (event) => {
@@ -124,12 +92,31 @@ searchForm.addEventListener('submit', (event) => {
 
     searchMeal(searchInput.value)
 
-    ingredientCard.style.display = "none"
-    categoryLink.style.display = "none"
     mealCard.style.display = "none"
     searchRowCard.style.display = "block"
     searchRowCard.removeAttribute('hidden')
     about.style.display = "none"
+})
+
+
+// click event for home link
+const homeLink = document.getElementById('home-link')
+homeLink.addEventListener('click', () => {
+    searchRowCard.style.display = "none"
+    mealCard.style.display = "block"
+    about.style.display = "none"
+    recipeCard.style.display = "block"
+    loadRandom()
+})
+
+// click event for Recipes link
+const recipeLink = document.getElementById('recipe-link')
+recipeLink.addEventListener('click', () => {
+    searchRowCard.style.display = "none"
+    mealCard.style.display = "none"
+    about.style.display = "none"
+    recipeCard.style.display = "block"
+    loadMenu()
 })
 
 
@@ -143,10 +130,10 @@ function creatingMeal (image, id, category, title, instructions, link) {
     rowDiv.classList.add('row')
 
     const cardDiv = document.createElement('div')
-    cardDiv.classList.add('col-5', 'card-body')
+    // cardDiv.classList.add('card-body')
 
     const imageDiv = document.createElement('div')
-    imageDiv.classList.add('col-7')
+    // imageDiv.classList.add('column')
 
     const mealImage = document.createElement('img')
     mealImage.classList.add('card-media','h-100' )
@@ -248,7 +235,7 @@ function loadMenu () {
 
             const mealElement = creatingMeal(image, id, category, title, instructions, link)
 
-            mealCard.appendChild(mealElement)
+            recipeCard.appendChild(mealElement)
             })
         })
 }
@@ -273,62 +260,6 @@ function loadRandom () {
         })
 }
 
-// creating ingredients element
-function creatingIngredients (ingredient, image, id) {
-    const rootDiv = document.createElement('div')
-    rootDiv.classList.add('card', 'u-clearfix', 'col-12', 'px-0', 'mb-3')
-
-    const rowDiv = document.createElement('div')
-    rowDiv.classList.add('row')
-
-    const cardDiv = document.createElement('div')
-    cardDiv.classList.add('card-body', 'col-6')
-
-    const imageDiv = document.createElement('div')
-    imageDiv.classList.add('col-7')
-
-    const mealImage = document.createElement('img')
-    mealImage.classList.add('card-media','h-100' )
-    mealImage.src = image
-    mealImage.objectFit = 'cover'
-    mealImage.float = 'right'
-
-    const mealId = document.createElement('span')
-    mealId.classList.add('card-number', 'card-circle', 'subtle')
-    mealId.innerText = id
-
-    const mealTitle = document.createElement('h2')
-    mealTitle.classList.add('card-title')
-    mealTitle.innerText = ingredient
-
-
-    cardDiv.appendChild(mealTitle)
-    cardDiv.appendChild(mealId)
-
-    // append image element to image div
-    imageDiv.appendChild(mealImage)
-
-    // append divs to row
-    rowDiv.appendChild(cardDiv)
-    rowDiv.appendChild(imageDiv)
-
-    // append the row to the root
-    rootDiv.appendChild(rowDiv)
-
-}
-
-
-function loadIngredients () {
-    fetch(INGREDIENT)
-        .then((response) => response.json())
-        .then(data => {
-            const mealData = data.meals
-            const ingrElement = mealData.map(
-                cat => creatingIngredients(cat.strMeal,cat.strMealThumb, cat.idMeal)
-            )
-            ingredientCard.append(...ingrElement)
-        })
-}
 
 // search data
 function searchMeal (meal) {
@@ -351,43 +282,3 @@ function searchMeal (meal) {
             searchRowCard.replaceChildren(...searchResults)
         })
 }
-
- // create category element
- const createCategory = (image, name) => {
-
-    const rootDiv = document.createElement('div')
-    rootDiv.classList.add('col-4', 'p-1')
-
-    const cardDiv = document.createElement('div')
-    cardDiv.classList.add('card', 'col-12', 'p-2')
-
-    const categoryImg = document.createElement('img')
-    categoryImg.classList.add('card-img-top')
-    categoryImg.src = image
-
-    const categoryTitle = document.createElement('h4')
-    categoryTitle.classList.add('card-title')
-    categoryTitle.innerText = name
-
-    // append title and image to card
-    cardDiv.appendChild(categoryImg)
-    cardDiv.appendChild(categoryTitle)
-
-    rootDiv.appendChild(cardDiv)
-
-    return rootDiv
-
-}
-
-    // load meal categories
-    const loadCategory = () => {
-        fetch(CATEGORY)
-            .then((response) => response.json())
-            .then((data) => {
-                const categoriesData = data.categories
-                const categoryElems = categoriesData.map(
-                    cat => createCategory(cat.strCategoryThumb, cat.strCategory)
-                )
-                categoryRow.append(...categoryElems)
-            })
-    }
